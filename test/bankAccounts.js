f27edcaa1edd284fba0655fcd22334c8d8b23262
Grayson Chao <graysonchao@berkeley.edu>
2014-07-09 11:41:23 -0700

    Corrected bankAccounts and their tests

--- test/bankAccounts.js
+++ test/bankAccounts.js
@@ -3,7 +3,7 @@ Lob = new Lob('test_0dc8d51e0acffcb1880e0f19c79b2f5b0cc');
 var Should;
 Should = require('should');
 /* jshint camelcase: false */
-describe('Bank Accounts', function () {
+describe('bank accounts', function () {
   describe('create', function () {
     it('should succeed with inline addresses', function (done) {
       Lob.bankAccounts.create({
