565e26a21b4f92e6fb079d4de6d96c4f00659352
Grayson Chao <graysonchao@berkeley.edu>
2014-07-09 11:42:50 -0700

    Corrected checks and their tests

--- test/checks.js
+++ test/checks.js
@@ -3,7 +3,7 @@ Lob = new Lob('test_0dc8d51e0acffcb1880e0f19c79b2f5b0cc');
 var Should;
 Should = require('should');
 /* jshint camelcase: false */
-describe('Checks', function () {
+describe('checks', function () {
   describe('create', function () {
     it('should succeed with default parameters', function (done) {
       Lob.checks.create({
